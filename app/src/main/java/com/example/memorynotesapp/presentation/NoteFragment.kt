be2339357afa4b11cda2d9b9ec4bf8f426a342e8
Robson <robson.v8@hotmail.com>
2023-08-13 01:29:04 -0300

    start mvvm structure

--- app/src/main/java/com/example/memorynotesapp/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/memorynotesapp/presentation/NoteFragment.kt
@@ -1,15 +1,22 @@
 package com.example.memorynotesapp.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.example.memorynotesapp.R
+import com.example.core.data.Note
+import com.example.memorynotesapp.framework.NoteViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NoteFragment : Fragment() {
+
+    private lateinit var viewModel: NoteViewModel
+    private var currentNote = Note("", "", 0L,0L)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +28,25 @@ class NoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this)[NoteViewModel::class.java]
+
         val checkButton = view.findViewById<FloatingActionButton>(R.id.checkButton)
+        val contentView = view.findViewById<EditText>(R.id.contentView)
+        val titleView = view.findViewById<EditText>(R.id.titleView)
+
         checkButton.setOnClickListener {
-            Navigation.findNavController(it).popBackStack()
+            if (contentView.text.toString() != "" || titleView.text.toString() != ""){
+                val time: Long = System.currentTimeMillis()
+                currentNote.title = titleView.text.toString()
+                currentNote.content = contentView.text.toString()
+                currentNote.updateTime = time
+                if (currentNote.id == 0L){
+                    currentNote.creationTime = time
+                }
+                viewModel.saveNote(currentNote)
+            } else {
+                Navigation.findNavController(it).popBackStack()
+            }
         }
     }
 
